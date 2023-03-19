47ef71508f682f30f79f41cd2e0f575ef9f5f33c
madalinefitzpatrick <madaline.fitzpatrick@gmail.com>
2023-03-19 00:52:07 -0400

    lab completed

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,22 @@
+import ipdb
+
 def oxford_comma(items):
-    return None
+        if len(items) == 1:
+            return ''.join(items)
+        elif len(items) == 2:
+            return " and ".join(items)
+        elif len(items) == 3:
+            return (f"{items[0]}, {items[1]}, and {items[2]}")
+        elif len(items) > 3:
+            last_item=items.pop()
+            new_string=", ".join(items)
+            add_and = (", and ")
+            final_string= new_string + add_and + last_item
+            return final_string
+            
+            
+
+ 
+
+# ipdb.set_trace()
+
